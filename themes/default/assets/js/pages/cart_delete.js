5d760703bdf29e0b432af7a8818b0ff0c0bf700c
y5mx <107808097+y5mx@users.noreply.github.com>
2024-01-06 21:50:43 +0300

    Add files via upload

---
+++ themes/default/assets/js/pages/cart_delete.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('body').on('click','.delete-button',function(e){e.preventDefault();$.ajax({url:baseUrl+'/cart/item/'+$(this).attr('data-item-id'),method:'Delete',data:{_token:_token,referre_page:'cart'},dataType:'json',}).done(function(data){Salla.event.createAndDispatch('cart::delete-item',{data:data,});location.reload();}).fail(laravel.ajax.errorHandler);});});
